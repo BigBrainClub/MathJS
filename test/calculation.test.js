9afb60f332c7b169362a96f166a530f18566893d
Parker Staszkiewicz <parker.staszkiewicz@mymail.champlain.edu>
2020-03-08 21:31:11 -0400

    Pow function

--- test/calculation.test.js
+++ test/calculation.test.js
@@ -1,4 +1,4 @@
-const {abs, sqrt} = require('../src/calculation');
+const {abs, pow, sqrt} = require('../src/calculation');
 
 test("Absolute value of 2 is 2", () => {
     expect(abs(-2)).toBe(2);
@@ -10,6 +10,14 @@ test("Absolute value of -1.414 is 1.414", () => {
 
 test(`Absolute value of ${Number.MIN_SAFE_INTEGER} is ${Number.MAX_SAFE_INTEGER}`, () => {
     expect(abs(Number.MIN_SAFE_INTEGER)).toBe(Number.MAX_SAFE_INTEGER);
+});
+
+test("(-2)^4 is 16", () => {
+    expect(pow(-2,4)).toBe(16);
+});
+
+test("(2)^10 is 1024", () => {
+    expect(pow(2,10)).toBe(1024);
 });
 
 test("Square Root of 4 is 2", () => {
